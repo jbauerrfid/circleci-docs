ef49a8d1c21708c8da7387942af5a79b3dba018b
nav <nramabad@gmail.com>
2021-12-30 01:48:25 -0800

    Feat: add experiment template

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -2,10 +2,12 @@ import forceAll from './forceAll';
 import relatedResources from './relatedResources';
 import surfaceFullExampleConfig from './surfaceFullExampleConfig';
 import videoTutorials from './videoTutorials';
+import languageGuides from './languageGuides';
 import './snippetFeedback';
 
 export default {
   forceAll,
+  languageGuides,
   relatedResources,
   surfaceFullExampleConfig,
   videoTutorials,
